20b6b759cc0446538061e2c47e647877549a268a
touyi <1074102503@qq.com>
2025-05-12 23:50:46 +0800

    config

--- src/config.js
+++ src/config.js
@@ -11,7 +11,14 @@ export const globalConfig = {
     "bot": {
         // 5 wsol, 最大用于购买的wsol数量, 单位lamports
         "maxInputAmount": 5000000000, 
-        "minProfit": 1000, // 最小利润, 单位lamports,如果交易最终利润小于该值，交易会报错
+        // 最小利润, 单位lamports,如果交易最终利润小于该值，交易会报错
+        "minProfit": 1000, 
+        // 最大交易发送数率，每秒发送的最大交易数，用于控制交易发送的速度
+        "maxSendRate": 10,
+        // 单进程异步并发数量，用于控制异步构造交易的并发数量，主要是异步IO，在js中始终无法超过单线程
+        "maxIOConcurrent": 10,
+        // 进程并发数量，用于控制进程的并发数量，会启动多个进程
+        // "maxProcessConcurrent": 2,
     },
     // 需要监听套利的mint列表
     "mintList": [
@@ -19,7 +26,7 @@ export const globalConfig = {
     ],
     // address_lookup_tables,
     "address_lookup_tables": [
-        // 这个alt提供了一些交易池的公共地址（例如Program，authority等），以减少交易的体积, 也可以替换为其他的
+        // 这个alt提供了一些交易池的公共地址（例如Program，authority等），以减少交易的体积, 也可以替换为其他的，支持多个
         "8xXRCZ18hs6dpwkgLMasgRoZStPRPJi2Dg6z54kSiWTE",
     ],
     
